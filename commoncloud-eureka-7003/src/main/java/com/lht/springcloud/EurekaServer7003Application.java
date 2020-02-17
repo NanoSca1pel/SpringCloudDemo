2020302067199d0e1d474c427538eacd350b741d
NanoSca1pel <lhtao846321@163.com>
2020-02-17 13:19:39 +0800

    eureka构建

---
+++ commoncloud-eureka-7003/src/main/java/com/lht/springcloud/EurekaServer7003Application.java
@@ -0,0 +1,18 @@
+package com.lht.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author lhtao
+ * @date 2019/8/16 16:29
+ */
+@SpringBootApplication
+@EnableEurekaServer //EurekaServer服务器端启动类，接受其他微服务注册进来
+public class EurekaServer7003Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(EurekaServer7003Application.class, args);
+    }
+}
